37b47db49e46e5ed9513650a1939af128c5ea606
Beatriz Terra Sakai <beatriz.sakai@bs.nttdata.com>
2025-07-28 12:54:19 +0000

    Cria a view do módulo de vendas

---
+++ app/vendas/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("vendas.controller.View1", {
+        onInit() {
+        }
+    });
+});
